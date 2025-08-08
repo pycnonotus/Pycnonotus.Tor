0b3f85716616604c1b7af88fa67ead1274b010c7
Anton Golovachev <anton.golo.fh@gmail.com>
2025-08-08 22:33:10 +0300

    Bump version to 1.1.0 and add implicit operators and property checks in `Tor`.

--- Pycnonotus.Tor/Tor.cs
+++ Pycnonotus.Tor/Tor.cs
@@ -5,9 +5,17 @@ public class Tor<T, Other>
     public T First { get; }
     public Other Second { get; }
 
+    public  bool IsFirst => First is not null;
+    public bool IsSecond => Second is not null;
+    
     public Tor(T first, Other second)
     {
         First = first;
         Second = Second;
     }
+    
+    public static implicit operator Tor<T, Other>(T first) => new Tor<T, Other>(first, default);
+    public static implicit operator Tor<T, Other>(Other second) => new Tor<T, Other>(default, second);
+    
+    
 }
