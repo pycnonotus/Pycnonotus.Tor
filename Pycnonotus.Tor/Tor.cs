74b269ba378a62fbe1a9a503d292065ccc25df24
Anton Golovachev <anton.golo.fh@gmail.com>
2025-08-08 22:42:38 +0300

    Bump version to 1.2.0 and enable nullable reference types with updated implicit operators in `Tor`.

--- Pycnonotus.Tor/Tor.cs
+++ Pycnonotus.Tor/Tor.cs
@@ -1,9 +1,10 @@
-﻿namespace Pycnontus;
+﻿#nullable enable
+namespace Pycnontus;
 
 public class Tor<T, Other>
 {
-    public T First { get; }
-    public Other Second { get; }
+    public T? First { get; }
+    public Other? Second { get; }
 
     public  bool IsFirst => First is not null;
     public bool IsSecond => Second is not null;
@@ -18,7 +19,7 @@ public class Tor<T, Other>
     public static implicit operator Tor<T, Other>(Other second) => new Tor<T, Other>(default, second);
     
     
-    public static implicit operator T(Tor<T, Other> tor) => tor.First;
-    
+    public static implicit operator T?(Tor<T, Other> tor) => tor.First;
+    public  static implicit operator Other?(Tor<T, Other> tor) => tor.Second;
     
 }
